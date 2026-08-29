d20ae2d7b85b3499147dddfe0b3216e23005ee45
Kavi Bharathi P <kavibarathi643@gmail.com>
2026-08-29 13:52:06 +0530

    Add Easy Java Solution for 268. Missing Number from LeetCode

---
+++ LeetCode/Patterns/Binary Search/268. Missing Number/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int k=0;
+        for (int i=0;i<nums.length-1;i++){
+            if(nums[i+1]!=nums[i]+1){
+                return nums[i]+1;
+            }
+        }
+        return nums[nums.length-1]+1;
+    }
+}
