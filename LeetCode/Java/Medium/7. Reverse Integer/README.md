42fac5eeff40910ba0eac6cc8fd1273663120793
Kavi Bharathi P <kavibarathi643@gmail.com>
2026-09-01 10:26:01 +0530

    Add Medium Java Solution for 7. Reverse Integer from LeetCode

--- LeetCode/Java/Medium/7. Reverse Integer/README.md
+++ LeetCode/Java/Medium/7. Reverse Integer/README.md
@@ -1,6 +1,6 @@
 # 📝 7. Reverse Integer (LeetCode)
 
-🔗 [Problem Link](https://leetcode.com/problems/reverse-integer/?envType=problem-list-v2&envId=math)
+🔗 [Problem Link](https://leetcode.com/problems/reverse-integer/)
 
 ![Difficulty](https://img.shields.io/badge/Difficulty-Medium-orange) ![Language](https://img.shields.io/badge/Language-Java-blue)
 
@@ -8,8 +8,8 @@
 Math
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 82 ms
+- **Memory:** 42.8 MB
 
 ---
 
--- LeetCode/Java/Medium/7. Reverse Integer/Solution.java
+++ LeetCode/Java/Medium/7. Reverse Integer/Solution.java
@@ -1,18 +1,22 @@
 class Solution {
     public int reverse(int x) {
-        int y=Math.abs(x);
-        int r=0;
-        int rev=0;
-        while(y>0){
-            r=y%10;
-            rev=(rev*10)+r;
-            y=y/10;
+        int rev = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 ||
+                (rev == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+            if (rev < Integer.MIN_VALUE / 10 ||
+                (rev == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
         }
-        if(rev<Math.pow(2,31)){
-        if(x<0){
-            return rev-(rev*2);
-        }
-        return rev;}
-        return 0;
+
+        return rev;
     }
 }
