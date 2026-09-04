ccccbb77e805d1d6321f7a52b9e59ca11cb1b443
Kavi Bharathi P <kavibarathi643@gmail.com>
2026-09-04 19:41:51 +0530

    Add Easy Java Solution for 350. Intersection of Two Arrays II from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/350. Intersection of Two Arrays II/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+public int[] intersect(int[] nums1, int[] nums2)        {
+ArrayList<Integer> al=new ArrayList<>();
+
+        for(int i:nums1){
+            int k=0;
+            for(int j:nums2){
+                if(i==j){
+                    al.add(i);
+                    k=1;
+                }
+                if(k==1){
+                    break;
+                }
+            }
+        }
+int[] arr = new int[al.size()];
+
+for (int i = 0; i < al.size(); i++) {
+    arr[i] = al.get(i); 
+}        return arr;
+    }
+}
