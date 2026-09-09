31f2da7d08fd7f1e3eb375b1f1f6785a7a708727
Kavi Bharathi P <kavibarathi643@gmail.com>
2026-09-09 16:02:31 +0530

    Add Easy Java Solution for 908. Middle of the Linked List from LeetCode

--- LeetCode/Java/Easy/908. Middle of the Linked List/Solution.java
+++ LeetCode/Java/Easy/908. Middle of the Linked List/Solution.java
@@ -15,7 +15,7 @@ class Solution {
         }
         ListNode slow=head;
         ListNode fast=head;
-        while(fast!=null && fast.next!=null){
+        while(fast.next!=null && fast!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
